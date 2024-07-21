1557f903edf431a935bb506b32d6633f33439787
KolyambusLopa <akuzolna@gmail.com>
2024-07-21 10:16:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('appointment-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const service = document.getElementById('service').value;
+    const master = document.getElementById('master').value;
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('time').value;
+
+    if (service && master && date && time) {
+        alert(`Вы записались на ${service} к ${master} на дату ${date} и время ${time}.`);
+        // Здесь можно добавить код для отправки данных на сервер или другие действия
+    } else {
+        alert('Пожалуйста, заполните все поля.');
+    }
+});
